4a374100690af048add72ae903c3b2a82f3236ed
Katedigi <muhammadyudi28022002@gmail.com>
2024-11-02 10:25:41 +0700

    Create reset.py

---
+++ src/reset.py
@@ -0,0 +1,10 @@
+x = input("Are u sure? (Y/n): ")
+if x.lower() =='y':
+    reset = """LICENS_KEY=
+    ID_INSTADDR=
+    PASSWORD_INSTADDR=
+    PASSWORD_PAYCO="""
+    open('.env', 'w', encoding='utf-8').write(reset)
+    print('Successfully reset!')
+else :
+    print("Canceled"
